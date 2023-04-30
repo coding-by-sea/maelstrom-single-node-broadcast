0aaa413f4e49d9cf8f2e421cf9fc95505cd1cb5a
Hang Wang <hw2437@columbia.edu>
2023-04-30 19:09:13 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+	var received []int
+	var topology map[string]any
+	n.Handle("echo", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		body["type"] = "echo_ok"
+		return n.Reply(msg, body)
+	})
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		body["type"] = "broadcast_ok"
+		num := body["message"].(float64)
+		received = append(received, int(num))
+		delete(body, "message")
+		return n.Reply(msg, body)
+	})
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		body["type"] = "read_ok"
+		body["messages"] = received
+		return n.Reply(msg, body)
+	})
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		body["type"] = "topology_ok"
+		topology = body["topology"].(map[string]any)
+		_ = topology
+		delete(body, "topology")
+		return n.Reply(msg, body)
+	})
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
